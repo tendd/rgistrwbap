9d4518a472488edf67ccabc82be7dbb7e9c4d0eb
tendd <82379586+tendd@users.noreply.github.com>
2021-04-12 16:37:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+nothing=[];
+function submit(){
+    var n1=document.getElementById("box1name1").value;
+    var n2=document.getElementById("box2name2").value;
+    var n3=document.getElementById("box3name3").value;
+    var n4=document.getElementById("box4name4").value;
+
+    nothing.push(n1);
+
+nothing.push(n2);
+
+nothing.push(n3);
+nothing.push(n4);
+console.log(nothing);
+}
